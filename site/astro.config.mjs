6081217be136f82b96c49822430dda8203e27a22
Jordan Cooper <jordan@cooperplanet.com>
2026-03-18 18:25:57 -0700

    Fix broken image links via astro config and script

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,7 +1,24 @@
 import { defineConfig } from 'astro/config'
+import { visit } from 'unist-util-visit'
+
+const GITHUB_PHOTOS_RE = /^https:\/\/raw\.githubusercontent\.com\/jordanfuzz\/blog\/master\/photos\/(.+)$/
+
+function remarkLocalPhotos() {
+    return (tree) => {
+        visit(tree, 'image', (node) => {
+            const match = node.url.match(GITHUB_PHOTOS_RE)
+            if (match) {
+                node.url = `/photos/${match[1].replace(/\.jpeg$/, '.jpg')}`
+            }
+        })
+    }
+}
 
 export default defineConfig({
-    site: 'https://jordan.cooperplanet.com', // update before deploying
+    site: 'https://jordan.cooperplanet.com',
     output: 'static',
     build: { format: 'directory' },
+    markdown: {
+        remarkPlugins: [remarkLocalPhotos],
+    },
 })
